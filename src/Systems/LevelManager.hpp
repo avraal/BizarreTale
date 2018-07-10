15b0666e1ec4db1265339af4044c4a5695b41b0e
avraal <gammaclone@gmail.com>
2018-07-10 17:12:51 +0300

    Prepare to ECS

---
+++ src/Systems/LevelManager.hpp
@@ -0,0 +1,17 @@
+
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+//
+// Created by Andrew Volski on 10.07.18.
+//
+
+#ifndef BIZARRETALE_LEVELMANAGER_HPP
+#define BIZARRETALE_LEVELMANAGER_HPP
+
+class LevelManager
+{
+
+};
+
+#endif //BIZARRETALE_LEVELMANAGER_HPP
---
+++ src/Systems/SRender.hpp
@@ -0,0 +1,19 @@
+
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+//
+// Created by Andrew Volski on 10.07.18.
+//
+
+#ifndef BIZARRETALE_SRENDER_HPP
+#define BIZARRETALE_SRENDER_HPP
+
+#include <SFML/Graphics.hpp>
+
+class SRender
+{
+
+};
+
+#endif //BIZARRETALE_SRENDER_HPP
