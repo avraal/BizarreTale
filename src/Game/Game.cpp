bb188020fd61e7150f3dd61353d9aba18e2a9059
avraal <gammaclone@gmail.com>
2018-08-20 19:00:35 +0300

    Class for Game representation

---
+++ src/Game/Game.cpp
@@ -0,0 +1,98 @@
+
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+//
+// Created by Andrew Volski on 20.08.18.
+//
+
+#include <dirent.h>
+#include <cstring>
+#include <iostream>
+#include "Game.hpp"
+#include "../Engine/CONST_DEFINITIONS.h"
+#include "../Engine/MapEditor.h"
+Game::Game(const std::string &title)
+{
+    this->Title = title;
+    levelManager = std::make_unique<SLevelManager>();
+    std::shared_ptr<MapEditor> editor = std::make_shared<MapEditor>(0, "MapEditor"); //ToDo: Add generate ID for Level's
+    levelManager->registerLevel(editor);
+}
+
+bool Game::start()
+{
+    window.create(sf::VideoMode(WINDOW_SIZE_HD_WIDTH, WINDOW_SIZE_HD_HEIGHT), Title);
+    MainCamera = window.getView();
+    MainCamera.setCenter(300, 320);
+
+    CurrentLevel = levelManager->loadLevel("MapEditor");
+    CurrentLevel->ImageDirectory = this->ImageDirectory;
+    if (!CurrentLevel->prepareLevel(window))
+    {
+        return false;
+    }
+    CurrentLevel->setCamera(MainCamera);
+    window.setKeyRepeatEnabled(true);
+    window.setVerticalSyncEnabled(true);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        window.clear(sf::Color(42, 76, 61));
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            CurrentLevel->MouseCallbacks(window, event);
+            CurrentLevel->KeyBoardCallbacks(window, event);
+            CurrentLevel->HandleGUIEvent(event);
+        }
+
+        window.setView(MainCamera);
+
+        CurrentLevel->draw(window);
+        window.display();
+    }
+    return true;
+}
+
+bool Game::findAllFiles(std::vector<std::string> &Container, std::vector<std::string> FileFormats)
+{
+    bool result = false;
+    DIR *dir;
+    dirent *directory;
+    if (FileFormats.empty())
+    {
+        FileFormats.emplace_back(".*");
+    }
+    if ((dir = opendir(ImageDirectory.c_str())) != nullptr)
+    {
+        while ((directory = readdir(dir)) != nullptr)
+        {
+            char *last = strrchr(directory->d_name, '.');
+            if (last != nullptr)
+            {
+                for (const auto &f : FileFormats)
+                {
+                    if (strcmp(last, f.c_str()) == 0)
+                    {
+                        std::cout << ImageDirectory << directory->d_name << " added to stack" << std::endl;
+                        Container.push_back(ImageDirectory + directory->d_name);
+                        result = true;
+                    }
+                }
+            }
+        }
+        closedir(dir);
+    } else
+    {
+        std::cerr << "Can't open a dir" << std::endl;
+    }
+    return result;
+}
+
--- src/Game/Game.hpp
+++ src/Game/Game.hpp
@@ -11,14 +11,26 @@
 
 #include <string>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include "../Engine/Systems/SLevelManager.hpp"
 
 class Game final
 {
 private:
     sf::RenderWindow window;
     sf::View MainCamera;
+    std::string Title;
+
+    std::unique_ptr<SLevelManager> levelManager;
+    std::shared_ptr<Level> CurrentLevel;
+    std::vector<std::string> PathToImages;                          //all images
+    std::vector<std::string> ImagesFormats;                         //all supported image formats
+
+    bool findAllFiles(std::vector<std::string> &Container, std::vector<std::string> FileFormats);
 public:
-    Game(const std::string title);
+    Game(const std::string &title);
+    bool start();
+    std::string ImageDirectory;
+
 };
 
 #endif //BIZARRETALE_GAME_HPP
