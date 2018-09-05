2047a37303be42de88f1201d29f2811e4d8f3475
avraal <gammaclone@gmail.com>
2018-09-05 18:56:07 +0300

    added checking OS

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 // This is an independent project of an individual developer. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
+#ifdef __linux__
+#define OS "LINUX"
+#else
+#define OS "UNSUPPORTED OS"
+#endif
+
 #include <iostream>
-#include <fstream>
-#include <SFML/Graphics.hpp>
 #include <TGUI/TGUI.hpp>
-#include <chrono>
 #include "Game/Game.hpp"
 #include "Engine/Components/CTile.h"
 #include "Engine/MapEditor.h"
 
+bool prepare();
 
 int main(int argc, char **argv)
 {
+    if (!prepare())
+    {
+        return 1;
+    }
     Game game("Bizarre Tale");
     game.ImageDirectory.clear();
     game.ImageDirectory.append(argv[0]);
@@ -23,4 +31,11 @@ int main(int argc, char **argv)
         return 1;
     }
     return 0;
+}
+
+bool prepare()
+{
+    std::cout << OS << std::endl;
+    return strcmp(OS, "UNSUPPORTED OS");
+//    return OS != "UNSUPPORTED OS";
 }
