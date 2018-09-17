e378ee4c9c6a5ad16e4de01ac6f1e7791ab18e3e
avraal <gammaclone@gmail.com>
2018-09-17 18:59:47 +0300

    CMakeLists.txt

---
+++ src/Game/Labyrinth/Labyrinth.hpp
@@ -0,0 +1,16 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+//
+// Created by Andrew Volski on 14.09.18.
+//
+
+#ifndef BIZARRETALE_LABYRINTH_HPP
+#define BIZARRETALE_LABYRINTH_HPP
+
+class Labyrinth
+{
+
+};
+
+#endif //BIZARRETALE_LABYRINTH_HPP
