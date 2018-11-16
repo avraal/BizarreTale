efa17c166f9655782e78d84233e20e71ddd7b26f
avraal <gammaclone@gmail.com>
2018-11-16 13:46:49 +0200

    fix OS detection

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,19 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 #ifdef __linux__
-#define OS "LINUX"
+    #define OS "LINUX"
 #elif __WIN32__
-#define OS "WIN32"
-#ifdef __WIN64__
-#define OS "WIN64"
-#endif
+    #ifdef __WIN64__
+        #define OS "WIN64"
+    #else
+        #define OS "WIN32"
+    #endif
 #else
-#define OS "UNSUPPORTED OS"
+    #define OS "UNSUPPORTED OS"
 #endif
 
 #include <iostream>
-#include <TGUI/TGUI.hpp>
 #include "Game/Game.hpp"
-#include "Engine/Components/CTile.hpp"
-#include "Engine/MapEditor.hpp"
 
 bool prepare();
 
@@ -42,5 +40,4 @@ bool prepare()
 {
     std::cout << OS << std::endl;
     return strcmp(OS, "UNSUPPORTED OS");
-    //    return OS != "UNSUPPORTED OS";
 }
